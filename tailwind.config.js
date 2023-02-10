3ea68e1cd8a8acb2cbbf71096d49ce7923953a2e
Demmyhub <parbdul@gmail.com>
2023-02-10 06:15:29 +0100

    Master

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,9 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        primary: '#ff6363',
-        secondary: {
-          100: '#e2e2d5',
-          200: '#888883',
-        }
-      },
-      fontFamily:{
-        body:['Bebas Neue','Itim']
-      }
+      
     },
   },
+  
   plugins: [],
 }
